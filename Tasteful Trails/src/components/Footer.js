6515a8b372c8a69d367b0d127649e6e99a0179fb
pawanraj295 <146716489+pawanraj295@users.noreply.github.com>
2024-04-30 14:18:00 +0530

    Add files via upload

---
+++ Tasteful Trails/src/components/Footer.js
@@ -0,0 +1,12 @@
+const Footer = () => {
+  return (
+    <footer className='p-8 flex flex-col  items-center gap-3 bg-rose-200 opacity-75'>
+      <h2 className='footer-logo font-bold lowercase italic text-2xl'>
+        Tasteful<span className='text-rose-500'>Trails</span>
+      </h2>
+      
+    </footer>
+  );
+};
+
+export default Footer;
